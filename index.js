90488a640927461f16af08fa683d6b7bbff02923
leandra <leandra.reid@gmail.com>
2018-12-17 15:46:57 -0500

    lots of updating

---
+++ index.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    });
+
+    $("#search-icon").on('click', function () {
+        $('#search-input').toggleClass('search-hidden search-showing', 1000, "easeOutSine")
+    });
+
+    $("select[name='make']").change(()=> {
+        $("select[name='model']").removeAttr("disabled");
+    });
+
+    const audiModels = ['A4', 'A8', 'A3'];
+    const bmwModels = ['2', 'M240i', 'M2'];
+    const chevroletModels = ['Bolt', 'Camaro', 'Equinox'];
+
+    $("#make").change(function () {
+        let car = this.value;
+        if (car == 'audi'){
+            audiModels.map(car => {
+                $('#model').append(
+                    `<option value=${car}>${car}</option>`
+                )
+            })
+        } else if (car == 'bmw'){
+            bmwModels.map(car => {
+                $('#model').append(
+                    `<option value=${car}>${car}</option>`
+                )
+            })
+        } else if (car == "chevrolet"){
+            chevroletModels.map(car => {
+                $('#model').append(
+                    `<option value=${car}>${car}</option>`
+                )
+            })
+        }
+    });
+
+    $('#searchModal').on('shown.bs.modal', function () {
+        console.log('hello')
+        $('#myInput').trigger('focus')
+    })
+
+
+
+
+
+});
+
